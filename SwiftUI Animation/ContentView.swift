51c6b002de2f86442b29e3b5f269e2b761f83ac4
Vishnu Sasikumar <vishnu.sasikumar@bidone.co.nz>
2021-06-14 09:17:38 +1200

    Initial Commit

---
+++ SwiftUI Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Animation
+//
+//  Created by Vishnu Sasikumar on 14/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI Animation
+//
+//  Created by Vishnu Sasikumar on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
